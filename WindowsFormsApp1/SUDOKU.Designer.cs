7b73ba8a9b270e8a7da35cff1f576bf8ecb17855
LIBIA71421 <73616994+LIBIA71421@users.noreply.github.com>
2020-11-11 21:03:11 -0400

    Add files via upload

---
+++ WindowsFormsApp1/SUDOKU.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class SUDOKU
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_verif = new System.Windows.Forms.Button();
+            this.btn_nuevo = new System.Windows.Forms.Button();
+            this.btn_nivel = new System.Windows.Forms.Label();
+            this.rb_bajo = new System.Windows.Forms.RadioButton();
+            this.rb_intermedio = new System.Windows.Forms.RadioButton();
+            this.rb_alto = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(596, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.BackColor = System.Drawing.Color.Black;
+            this.btn_limpiar.Font = new System.Drawing.Font("Modern No. 20", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpiar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_limpiar.Location = new System.Drawing.Point(627, 122);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(143, 40);
+            this.btn_limpiar.TabIndex = 17;
+            this.btn_limpiar.Text = "LIMPIAR";
+            this.btn_limpiar.UseVisualStyleBackColor = false;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_verif
+            // 
+            this.btn_verif.BackColor = System.Drawing.Color.Black;
+            this.btn_verif.Font = new System.Drawing.Font("Modern No. 20", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_verif.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_verif.Location = new System.Drawing.Point(627, 46);
+            this.btn_verif.Name = "btn_verif";
+            this.btn_verif.Size = new System.Drawing.Size(143, 67);
+            this.btn_verif.TabIndex = 18;
+            this.btn_verif.Text = "VERIFICACION";
+            this.btn_verif.UseVisualStyleBackColor = false;
+            this.btn_verif.Click += new System.EventHandler(this.btn_verif_Click);
+            // 
+            // btn_nuevo
+            // 
+            this.btn_nuevo.BackColor = System.Drawing.Color.Black;
+            this.btn_nuevo.Font = new System.Drawing.Font("Modern No. 20", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_nuevo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_nuevo.Location = new System.Drawing.Point(627, 315);
+            this.btn_nuevo.Name = "btn_nuevo";
+            this.btn_nuevo.Size = new System.Drawing.Size(143, 75);
+            this.btn_nuevo.TabIndex = 19;
+            this.btn_nuevo.Text = "NUEVO JUEGO";
+            this.btn_nuevo.UseVisualStyleBackColor = false;
+            this.btn_nuevo.Click += new System.EventHandler(this.btn_nuevo_Click);
+            // 
+            // btn_nivel
+            // 
+            this.btn_nivel.AutoSize = true;
+            this.btn_nivel.Location = new System.Drawing.Point(650, 222);
+            this.btn_nivel.Name = "btn_nivel";
+            this.btn_nivel.Size = new System.Drawing.Size(38, 13);
+            this.btn_nivel.TabIndex = 20;
+            this.btn_nivel.Text = "NIVEL";
+            // 
+            // rb_bajo
+            // 
+            this.rb_bajo.AutoSize = true;
+            this.rb_bajo.Location = new System.Drawing.Point(642, 243);
+            this.rb_bajo.Name = "rb_bajo";
+            this.rb_bajo.Size = new System.Drawing.Size(46, 17);
+            this.rb_bajo.TabIndex = 21;
+            this.rb_bajo.TabStop = true;
+            this.rb_bajo.Text = "Bajo";
+            this.rb_bajo.UseVisualStyleBackColor = true;
+            // 
+            // rb_intermedio
+            // 
+            this.rb_intermedio.AutoSize = true;
+            this.rb_intermedio.Location = new System.Drawing.Point(642, 266);
+            this.rb_intermedio.Name = "rb_intermedio";
+            this.rb_intermedio.Size = new System.Drawing.Size(74, 17);
+            this.rb_intermedio.TabIndex = 22;
+            this.rb_intermedio.TabStop = true;
+            this.rb_intermedio.Text = "Intermedio";
+            this.rb_intermedio.UseVisualStyleBackColor = true;
+            // 
+            // rb_alto
+            // 
+            this.rb_alto.AutoSize = true;
+            this.rb_alto.Location = new System.Drawing.Point(642, 289);
+            this.rb_alto.Name = "rb_alto";
+            this.rb_alto.Size = new System.Drawing.Size(43, 17);
+            this.rb_alto.TabIndex = 23;
+            this.rb_alto.TabStop = true;
+            this.rb_alto.Text = "Alto";
+            this.rb_alto.UseVisualStyleBackColor = true;
+            // 
+            // SUDOKU
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rb_alto);
+            this.Controls.Add(this.rb_intermedio);
+            this.Controls.Add(this.rb_bajo);
+            this.Controls.Add(this.btn_nivel);
+            this.Controls.Add(this.btn_nuevo);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_verif);
+            this.Controls.Add(this.btn_limpiar);
+            this.Name = "SUDOKU";
+            this.Text = "SUDOKU";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_verif;
+        private System.Windows.Forms.Button btn_nuevo;
+        private System.Windows.Forms.Label btn_nivel;
+        private System.Windows.Forms.RadioButton rb_bajo;
+        private System.Windows.Forms.RadioButton rb_intermedio;
+        private System.Windows.Forms.RadioButton rb_alto;
+    }
+}
---
+++ WindowsFormsApp1/SUDOKU.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class SUDOKU : Form
+    {
+        public SUDOKU()
+        {
+            InitializeComponent();
+            createCells(9);
+            startNewGame();
+        }
+        private void startNewGame()
+        {
+            loadValues();
+            var hintsCount = 0;
+
+            if (rb_bajo.Checked)
+                hintsCount = 45;
+            else if (rb_intermedio.Checked)
+                hintsCount = 30;
+            else if (rb_alto.Checked)
+                hintsCount = 15;
+
+            tablero(hintsCount);
+        }
+
+        private void loadValues()
+        {
+            foreach (var cell in cells)
+            {
+                cell.Value = 0;
+                cell.Clear();
+            }
+            findValueForNextCell(0, -1);
+        }
+
+        Random random = new Random();
+        private bool findValueForNextCell(int i, int j)
+        {            
+            if (++j > 8)
+            {
+                j = 0;                // Exit if the line ends
+                if (++i > 8)
+                    return true;
+            }
+            var value = 0;
+            var numsLeft = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            do{    
+                               
+                if (numsLeft.Count < 1)
+                {
+                    cells[i, j].Value = 0;
+                    return false;
+                }
+                // Toma un número aleatorio de los números que quedan en la lista                
+                value = numsLeft[random.Next(0, numsLeft.Count)];
+                cells[i, j].Value = value;
+
+                numsLeft.Remove(value);
+            }while (!isValidNumber(value, i, j) || !findValueForNextCell(i, j));
+
+            cells[i, j].Text = value.ToString();
+
+            return true;
+        }
+
+        private bool isValidNumber(int value, int x, int y)
+        {
+            for (int i = 0; i < 9; i++)
+            {// dirección vertical              
+                if (i != y && cells[x, i].Value == value)
+                    return false;
+                // dirección horizontal                 
+                if (i != x && cells[i, y].Value == value)
+                    return false;
+            } // Verifica bloque            
+            for (int i = x - (x % 3); i < x - (x % 3) + 3; i++)
+            {
+                for (int j = y - (y % 3); j < y - (y % 3) + 3; j++)
+                {
+                    if (i != x && j != y && cells[i, j].Value == value)
+                        return false;
+                }
+            }
+            return true;
+        }
+        SudokuCell[,] cells = new SudokuCell[9, 9];
+
+        private void createCells(int tam)
+        {
+            for (int i = 0; i < tam; i++)
+            {
+                for (int j = 0; j < tam; j++)
+                {  // Create 81 cells for with styles and locations based on the index
+                    cells[i, j] = new SudokuCell();
+                    cells[i, j].Font = new Font(SystemFonts.DefaultFont.FontFamily, 20);
+                    cells[i, j].Size = new Size(40, 40);
+                    cells[i, j].ForeColor = SystemColors.ControlDarkDark;
+                    cells[i, j].Location = new Point(i * 40, j * 40);
+                    cells[i, j].BackColor = ((i / 3) + (j / 3)) % 2 == 0 ? SystemColors.Control : Color.LightGray;
+                    cells[i, j].FlatStyle = FlatStyle.Flat;
+                    cells[i, j].FlatAppearance.BorderColor = Color.Blue;
+                    cells[i, j].X = i;
+                    cells[i, j].Y = j;
+
+                    // Assign key press event for each cells
+                    cells[i, j].KeyPress += cell_keyPressed;
+                    panel1.Controls.Add(cells[i, j]);
+                }
+            }
+        }
+
+        private void cell_keyPressed(object sender, KeyPressEventArgs e)
+        {
+            var cell = sender as SudokuCell;
+            // Do nothing if the cell is locked
+            if (cell.IsLocked)
+                return;
+            int value;
+            // Add the pressed key value in the cell only if it is a number
+            if (int.TryParse(e.KeyChar.ToString(), out value))
+            {    
+                if (value == 0)
+                    cell.Clear();
+                else
+                    cell.Text = value.ToString();
+
+                cell.ForeColor = SystemColors.ControlDarkDark;
+            }
+        }
+
+        private void tablero(int hintsCount)
+        {
+            for (int i = 0; i < hintsCount; i++)
+            {
+                var rX = random.Next(9);      var rY = random.Next(9);
+                cells[rX, rY].Text = cells[rX, rY].Value.ToString();
+                cells[rX, rY].ForeColor = Color.Black;
+                cells[rX, rY].IsLocked = true;
+            }
+        }
+
+        private void btn_verif_Click(object sender, EventArgs e)
+        {
+            var wrongCells = new List<SudokuCell>();
+            // Encuentra todas las entradas incorrectas           
+            foreach (var cell in cells)
+            {
+                if (!string.Equals(cell.Value.ToString(), cell.Text))
+                {
+                    wrongCells.Add(cell);
+                }
+            }
+            // Comprueba si las entradas son incorrectas o si el jugador gana            
+            if (wrongCells.Any())
+            { // Resalta las entradas incorrectas                  
+                wrongCells.ForEach(x => x.ForeColor = Color.Red);
+                MessageBox.Show("Wrong inputs");
+            }
+            else
+            {
+                MessageBox.Show("You Wins");
+            }
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            foreach (var cell in cells)
+            {// Limpia la celda solo si no está bloqueada                
+                if (cell.IsLocked == false)
+                    cell.Clear();
+            }
+        }
+
+        private void btn_nuevo_Click(object sender, EventArgs e)
+        {
+            startNewGame();
+        }
+    }
+}
